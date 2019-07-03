39e2f79d6cdd9799b1d8fefe8f07202f591177ed
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-07-02 22:39:33 -0500

    updated with some comments.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-// Placing focus on "name"
+// Placing focus on "name".
 $('#name').focus();
 $("#other-title").hide();
 
-// If "other" is selected from job role, then show the text input.
 
+/////// JOB ROLE SECTION //////
+
+// If "other" is selected from job role, then show the text input.  Otherwise hide the text input.
 $('#title').on('change', function(){
     let option = $('#title').val();
     if(option=='other'){
@@ -11,31 +13,42 @@ $('#title').on('change', function(){
     }else{$("#other-title").hide();}
 });
 
-// Hiding the select theme option in design menu
+
+/////// T-SHIRT SECTION //////
+
+// Extra credit: Hide colors section.  Only show it when a design theme is selected.
+$("#colors-js-puns").hide();
+
+// Hide the select theme option in design menu
 $("#design option:first-child").hide();
 
-// Updating the color field to read "please select a T-shirt theme"
+// Update the color field to read "please select a T-shirt theme"
 $("#color").prepend('<option selected>Please select a T-shirt theme</option>');
 
 // Hiding the colors in the "color" drop down menu
 $('#color option').hide();
 
-// 
+// When JS Puns is selected in design section, then only show the options that has the word "JS puns".
+// Otherwise show options that contain "I" and hide options that contain "JS Puns".
+// When JS puns is selected, automatically select "Cornflower Blue" in.  Otherwise automatically select "Tomato".
+
 $("#design").on("change", function(){
     let design = $(this).val();
+    $("#colors-js-puns").show();
+
     if (design === "js puns"){
         $("#color option:contains('JS Puns')").show();
         $("#color option:contains('I')").hide();
-
+        $("#color option[value='cornflowerblue']").prop("selected",true);
     } else {
         $("#color option:contains('I')").show();
         $("#color option:contains('JS Puns')").hide();
+        $("#color option[value='tomato']").prop("selected",true);
     }
 });
 
 // activity section
 
-//let testNumber = parseInt($(".activities input:checked").text());
     $('.activities :checkbox').click(function(){
        let totalActivityCost = 0;
        const $jsFrameWorks = $('input[name="js-frameworks"]');
@@ -68,9 +81,12 @@ $("#design").on("change", function(){
         $(".activities").append('<p id="total-cost"></p>');
         $('#total-cost').html('Total Cost: $'+totalActivityCost);
 
-        // Disabling conflicting
-      $jsFrameWorks.change(function() {
-        if ($(this).val(":checked")){
+    // Disabling conflicting
+    // Disabling conflicting options.
+
+
+        $jsFrameWorks.change(function() {
+        if ($(this).is (":checked")){
             $express.prop("disabled", true);
             $express.parent().css({color: "grey"});
         } else{
@@ -78,8 +94,8 @@ $("#design").on("change", function(){
             $express.parent().css({color: "black"});
         }
         });
-      $express.change(function() {
-        if ($(this).val(":checked")){
+        $express.change(function() {
+        if ($(this).is (":checked")){
             $jsFrameWorks.prop("disabled", true);
             $jsFrameWorks.parent().css({color: "grey"});
         } else{
@@ -87,9 +103,9 @@ $("#design").on("change", function(){
             $jsFrameWorks.parent().css({color: "black"});
 
         }
-      });
-      $jslibs.change(function() {
-        if ($(this).val(":checked")){
+        });
+        $jslibs.change(function() {
+        if ($(this).is (":checked")){
             $node.prop("disabled", true);
             $node.parent().css({color: "grey"});
         } else{
@@ -97,17 +113,20 @@ $("#design").on("change", function(){
             $node.parent().css({color: "black"});
 
         }
-      });
-      $node.change(function() {
-        if ($(this).val(":checked")){
+        });
+        $node.change(function() {
+        if ($(this).is (":checked")){
             $jslibs.prop("disabled", true);
             $jslibs.parent().css({color: "grey"});
         } else{
             $jslibs.prop("disabled", false);
             $jslibs.parent().css({color: "black"});
         }
-      });
-   });
+        });
+        $(".activities").append('<p id="total-cost"></p>');
+        $('#total-cost').html('Total Cost: $'+totalActivityCost);
+    });
+    
 
 
    // payment section
@@ -122,31 +141,26 @@ $("#design").on("change", function(){
    $("#payment").on("change", function(){
 
     payment = $(this).val();
-
     console.log("payment is " + payment);
-
     
        if (payment === "bitcoin"){
-        $('div p').eq(0).hide();
-        $('.credit-card').hide();
-       } else { 
-        $('div p').eq(0).show();
-        $('.credit-card').show();
-       }
-
+            $('div p').eq(0).hide();
+            $('.credit-card').hide();
+            $('div p').eq(1).show();
+       } 
+       
        if (payment === "paypal"){
-        $('div p').eq(1).hide();
-        $('.credit-card').hide();
-       } else { 
-        $('div p').eq(1).show();
-       }
-
+            $('div p').eq(1).hide();
+            $('.credit-card').hide();
+            $('div p').eq(0).show();
+       } 
+       
        if (payment === "credit card"){
-        console.log('credit card selected');
-        $('div p').hide();
+            console.log('credit card selected');
+            $('div p').hide();
+            $('.credit-card').show();
        }
-    
-   });
+    });
 
 
 
@@ -158,14 +172,17 @@ $("#design").on("change", function(){
 function checkName(){
     const nameReg = /^[A-Za-z]+$/;
     const name = $('#name').val();
-    //nameError = false;
+
+    // Global variable to check whether error in name exists.  
+    // If false = no error, if true = error exists.
+    nameError = false;
 
     if (!nameReg.test(name) || name == ""){
         console.log("name should turn red");
         $('input[name="user_name"]').css("border-color", "#FF0000");
         $('#name').prev().css("color", "#FF0000");
-        //nameError = true;
-    } else {//nameError = false;
+        nameError = true;
+    } else {
         $('input[name="user_name"]').css("border-color", "#b0d3e2");
         $('#name').prev().css("color", "#184f68");
     }
@@ -174,17 +191,16 @@ function checkName(){
 function checkEmail(){
     const emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     const email = $('#mail').val();
-    //emailError = false;
+    emailError = false;
 
     if (!emailReg.test(email) || email == ""){
         console.log("email should turn red");
         $('input[name="user_email"]').css("border-color", "#FF0000");
         $('#mail').prev().css("color", "#FF0000");
-        //emailError = true;
+        emailError = true;
     } else {
         $('input[name="user_email"]').css("border-color", "#b0d3e2");
         $('#mail').prev().css("color", "#184f68");
-       // emailError = false;
     }
 }  
 
@@ -194,13 +210,12 @@ function checkActivities(){
         activityNumber.push($(this).text());
     });
 
-    //activityError = false;
+    activityError = false;
     if (activityNumber.length < 1){
         $('.activities legend').css("color", "red");
-        //activityError = true;
+        activityError = true;
     } else {
         $('.activities legend').css("color", "#184f68");
-       // activityError = false;
     }
 }
 
@@ -212,7 +227,7 @@ function zip (){
         $('#zip').css('border-color', "red");
         $('#zip').prev().css('color', "red");
 
-        //cardError = true;
+        cardError = true;
 } else {
     $('#zip').css('border-color', "black");
     $('#zip').prev().css('color', "black");
@@ -229,7 +244,7 @@ function cvv (){
         $('#cvv').css('border-color', "red");
         $('#cvv').prev().css('color', "red");
 
-        //cardError = true;
+        cardError = true;
     } else{
         $('#cvv').css('border-color', "black");
         $('#cvv').prev().css('color', "black");
@@ -239,14 +254,14 @@ function cvv (){
 function cardNumber (){
     let cardNumberReg = /^[0-9]{13,16}?$/;
     const cardNumberVal = $("#cc-num").val();
-    //cardError = false;
+    cardError = false;
 
     if (!cardNumberReg.test(cardNumberVal)){
         console.log("cardNumber turns red");
         $('#cc-num').css('border-color', "red");
         $('#cc-num').prev().css('color', "red");
 
-        //cardError = true;
+        cardError = true;
     } else{
         $('#cc-num').css('border-color', "black");
         $('#cc-num').prev().css('color', "black");
@@ -261,33 +276,9 @@ function checkCard(){
         cvv();
         zip();
         
-    } //else {cardError = true;
-    //}
+    } 
 }
 
-
-// const creditCard = () => {
-//     let cardNumber = $("#cc-num").val();
-//     let cardZip = $("#zip").val();
-//     let cardCvv = $("#cvv");
-//     creditCardError = false;
-
-//     if (cardNumber.length < 13 || cardNumber.length > 16){
-//         $('cc-num').css("color", "red");
-//         creditCardError = true;
-//     } else if (cardZip.length !== 5){
-//         $('cc-num').css("color", "red");
-//         return true;
-//     } else if (cardCvv.length === 3){
-//         $('cvv').css("color", "red");
-//         creditCardError = true;
-//     } else{
-//         $('cvv').css("color", "#black");
-//         $('cc-num').css("color", "black");
-//         $('cvv').css("color", "black");
-//         creditCardError = false;
-//     }
-// }
 
 // const creditCard = () => {
 //     let cardNumber = $("#cc-num").val();
@@ -321,26 +312,9 @@ function checkCard(){
 // }
 
 
-
-
-// $('button [type="submit"]').submit(function(){
-
-// nameError = false;
-// emailError = false;
-
-// checkName();
-
-// if (errorName === false){
-//     alert("name right");
-//     return true;
-// } else{
-//     alert("name wrong");
-//     return false;
-// }
-// });
-
+// No action will be taken until DOM is ready
 $(document).ready(function(){
-    $('button').click(function(){
+    $('button:submit').click(function(){
         event.preventDefault();
         console.log("Form submitted");
 
@@ -349,15 +323,13 @@ $(document).ready(function(){
         checkActivities();
         checkCard();
         
-        
-        // if (nameError || emailError || activityError || cardError){
-        //     console.log("error exist");
-        //     return true;
-        // } 
-        // else{
-        //     console.log("no error");
-        //     return false;
-        // }  
+        if (nameError || emailError || activityError || cardError){
+            console.log("error exist");
+            alert("Registration unsuccessul. Please correct the inputs marked red.")
+        } 
+        else{
+            console.log("no error");
+            alert("Registration successful! See ya at the conference!")
+        }  
     });
 });
-
